hvxl/otmonitor

commit 888a1d2df87997e9b3bb805c858889f3c2d6a58b

Return the result of commands via MQTT if the caller specified a response topic.

otmonitor.vfs/mqtt.tcl
@@ -180,7 +180,12 @@ proc mqttpub {topic data {qos 1} {retain 0}} {
 		set msg [dict get $data value]
 	    }
 	}
-	$mqtt publish $topic $msg $qos $retain
+	if {[dict exists $data properties] && [$mqtt configure -protocol] >= 5} {
+	    $mqtt publish -properties [dict get $data properties] \
+	      $topic $msg $qos $retain
+	} else {
+	    $mqtt publish $topic $msg $qos $retain
+	}
     } on error {err info} {
 	puts stderr [dict get $info -errorinfo]
     }
@@ -206,8 +211,14 @@ proc mqttstatus {topic data retained {prop {}}} {
     }
 }
 
-proc mqttaction {topic data args} {
-    global cfg mqttactions
+proc mqttaction {topic data retain props args} {
+    global cfg mqttactions actionid
+    if {[dict exists $props ResponseTopic]} {
+	# To be able to provide a response, the proc must run as a coroutine
+	if {[info coroutine] eq ""} {
+	    tailcall coroutine mqttaction[incr actionid] {*}[info level 0]
+	}
+    }
     set name [lindex [split $topic /] end]
     if {[dict exists $mqttactions $name]} {
 	lassign [dict get $mqttactions $name] arg cmd
@@ -241,7 +252,16 @@ proc mqttaction {topic data args} {
 	    return
 	}
     }
-    sercmd $cmd=$value "via MQTT"
+    set rc [sercmd $cmd=$value "via MQTT"]
+    if {[dict exists $props ResponseTopic]} {
+	set topic [dict get $props ResponseTopic]
+	set data [dict create value [list $rc] def {response string}]
+	# Return the correlation data, if any
+	if {[dict exists $props CorrelationData]} {
+	    dict set data properties [dict filter $props key CorrelationData]
+	}
+	mqttpub $topic $data $cfg(mqtt,qos)
+    }
 }
 
 mqttinit

otmonitor.vfs/docroot/cfgmqtt.html.tmpl
@@ -102,9 +102,9 @@ function mqtt(w) {
 <td>Quality of Service:</td>
 <td colspan="2">
 <select id="mqtt,qos" onchange="input(this, 'mqtt', 'qos')">
-<option [selected cfg(mqtt,qos) 0]>Fire and forget</option>
-<option [selected cfg(mqtt,qos) 1]>Acknowledged delivery</option>
-<option [selected cfg(mqtt,qos) 2]>Assured delivery</option>
+<option [selected cfg(mqtt,qos) 0]>0: Fire and forget</option>
+<option [selected cfg(mqtt,qos) 1]>1: Acknowledged delivery</option>
+<option [selected cfg(mqtt,qos) 2]>2: Assured delivery</option>
 </select>
 </td>
 </tr>

otmonitor.vfs/lib/tcl8/8.6/mqtt-4.0.tm
@@ -1149,7 +1149,7 @@ oo::class create mqtt {
 
     method bindata {{name ""}} {
 	lassign [payload Su] len
-	lassign [payload cu$len $name] data
+	lassign [payload a$len $name] data
 	return -count [incr len 2] $data
     }
 

commit 2aaff68770465ae9f857b949811a2dda13972f08

Fix MQTT actions, broken by commit 888a1d2df87997e9b3bb805c858889f3c2d6a58b (reported as issue #23).

otmonitor.vfs/mqtt.tcl
@@ -211,7 +211,7 @@ proc mqttstatus {topic data retained {prop {}}} {
     }
 }
 
-proc mqttaction {topic data retain props args} {
+proc mqttaction {topic data retain {props {}} args} {
     global cfg mqttactions actionid
     if {[dict exists $props ResponseTopic]} {
 	# To be able to provide a response, the proc must run as a coroutine